950f3e1172b2003e3e10e16881590cad272da17a
Felipe Silva <felipesilva2004@hotmail.com>
2024-03-10 19:20:10 -0300

    chore: remove unnecessary files and create movies page

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,9 +1,11 @@
 'use client';
 import { Button } from 'antd';
 import { useState } from 'react';
+import { useRouter } from 'next/router'
 
 export default function Home() {
   const [count, setCount] = useState(0);
+  const router = useRouter();
 
   const handleClick = () => {
     setCount(prevCount => prevCount + 1);
@@ -11,9 +13,10 @@ export default function Home() {
   
   return (
     <>
-      <h1>Hello, World!</h1>
+      <h1>Dashboard</h1>
       <Button onClick={handleClick} type="primary">Button</Button>
       <p>You clicked {count} times</p>
+      <Button onClick={() => router.push('/movies')} type="primary">Go to Movies</Button>
     </>
   );
 }
---
+++ frontend/src/pages/movies.jsx
@@ -0,0 +1,7 @@
+'use client';
+
+export default function Movies() {
+  return (
+    <h1>Movies</h1>
+  );
+}
