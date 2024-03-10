91335450021765f9a4f63f466a91fbccff0acc33
Felipe Silva <felipesilva2004@hotmail.com>
2024-03-10 12:34:58 -0300

    chore: manual test db creation with movies endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,9 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify
 from config import Config
 from utils.ingest_csv import ingest_csv_to_db
-from models import db
+from models import db, Movie
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,6 +18,21 @@ load_dotenv()
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/movies')
+def list_movies():
+    movies = Movie.query.all()
+    movies_info = []
+    for movie in movies:
+        movies_info.append({
+            "title": movie.title,
+            "year": movie.year,
+            "studios": [studio.name for studio in movie.studios],
+            "producers": [producer.name for producer in movie.producers],
+            "winner": any(award.is_winner for award in movie.awards)
+        })
+    return jsonify(movies_info)
+
 
 if __name__ == '__main__':
     csv_path = os.getenv('CSV_PATH')
