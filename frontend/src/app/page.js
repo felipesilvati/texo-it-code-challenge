d7050a7581eefb38dd06b6a065ca30165fe48324
Felipe Silva <felipesilva2004@hotmail.com>
2024-03-10 18:59:59 -0300

    chore: install antd and test reactiveness

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,19 @@
+'use client';
+import { Button } from 'antd';
+import { useState } from 'react';
+
 export default function Home() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(prevCount => prevCount + 1);
+  }
+  
   return (
-    <div>Hello World</div>
+    <>
+      <h1>Hello, World!</h1>
+      <Button onClick={handleClick} type="primary">Button</Button>
+      <p>You clicked {count} times</p>
+    </>
   );
 }
