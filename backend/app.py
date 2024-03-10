dab884f2ee12b02657ec2b4a4ab7173dd6ab1236
Felipe Silva <felipesilva2004@hotmail.com>
2024-03-09 21:40:58 -0300

    feat: init in memory db

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from models import db
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def hello_world():
