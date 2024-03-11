91ef997426cd3baa917138254a2fdf68246a7306
Felipe Silva <felipesilva2004@hotmail.com>
2024-03-11 16:49:27 -0300

    fix: search input and button functionality

--- frontend/src/components/ListMovieWinnersByYear.jsx
+++ frontend/src/components/ListMovieWinnersByYear.jsx
@@ -1,22 +1,22 @@
 'use client';
 import React, { useState } from 'react';
-import { Button, Typography, Table, Card, Input } from 'antd';
+import { Button, Typography, Table, Card, InputNumber, Space } from 'antd';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { BASE_API_URL } from '@/utils/constants';
-
 const { Text } = Typography;
 
 export default function ListMovieWinnersByYear() {
   const [searchYear, setSearchYear] = useState('');
   const [searchActive, setSearchActive] = useState(false);
+  const isSearchYearValid = !!searchYear && !isNaN(searchYear) && parseInt(searchYear, 10) > 0;
   const { data, isLoading, isError } = useQuery({
     queryFn: () => axios
       .get(`${BASE_API_URL}/?winner=true&year=${searchYear}`)
       .then((res) => res.data),
     queryKey: ['listMovieWinnersByYear', searchYear],
     onError: (error) => console.error(error),
-    enabled: !!searchYear && !isNaN(searchYear) && parseInt(searchYear, 10) > 0 && searchActive,
+    enabled: isSearchYearValid && searchActive,
     onSettled: () => setSearchActive(false),
     retry: false,
     keepPreviousData: true,
@@ -34,8 +34,10 @@ export default function ListMovieWinnersByYear() {
 
   return (
     <Card style={{ marginTop: 16, width: 550 }} title='List movie winners by year'>
-      <Input onChange={(e) => setSearchYear(e.target.value)} value={searchYear} placeholder='Search by year' />
-      <Button type='primary' onClick={() => setSearchActive(true)}>Search</Button>
+      <Space.Compact style={{ width: '100%', marginBottom: 16 }}>
+        <InputNumber style={{ width: '100%' }} min={1} max={3000} defaultValue={1990} onChange={setSearchYear} value={searchYear} placeholder='Search by year' />
+        <Button type='primary' onClick={() => setSearchActive(true)} disabled={!isSearchYearValid}>Search</Button>
+      </Space.Compact>
       <Table
         style={{ width: 500 }}
         dataSource={data}
