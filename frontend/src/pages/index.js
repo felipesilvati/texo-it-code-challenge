073836dc617e7b85275c20e63028efe834cae383
Felipe Silva <felipesilva2004@hotmail.com>
2024-03-11 13:57:31 -0300

    feat: fetch data from api

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,12 +1,32 @@
 'use client';
 import Layout from '@/components/layout';
 import { Typography } from 'antd';
+import { useQuery, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import axios from 'axios';
 const { Text } = Typography;
 
-export default function Home() {
+const baseApiUrl = 'https://tools.texoit.com/backend-java/api/movies'
+
+export default function App() {
+  const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Dashboard />
+    </QueryClientProvider>
+  );
+
+}
+
+function Dashboard() {
+  const yearsWithMultipleWinners = useQuery({
+    queryFn: () => axios.get(baseApiUrl + '/?projection=years-with-multiple-winners'),
+    queryKey: ['yearsWithMultipleWinners'],
+  })
+
   return (
     <Layout>
       <Text>Dashboard Page Here</Text>
+      <div>{JSON.stringify(yearsWithMultipleWinners?.data)}</div>
     </Layout>
   );
 }
