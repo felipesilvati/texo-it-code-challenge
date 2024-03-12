cda6113cde6df6de55298aa757af59aa844dc2df
Felipe Silva <felipesilva2004@hotmail.com>
2024-03-12 14:03:54 -0300

    feat: search by year with react-highlight and antd search icon

--- frontend/src/pages/movies.js
+++ frontend/src/pages/movies.js
@@ -1,10 +1,12 @@
 'use client';
-import React, { useState } from 'react';
-import { Typography, Table, Card, Spin } from 'antd';
+import React, { useRef, useState } from 'react';
+import { Button, Space, Input, Typography, Table, Card, Spin } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { BASE_API_URL } from '@/utils/constants';
 import Layout from '@/components/Layout';
+import Highlighter from 'react-highlight-words';
 import { constructQueryString } from '@/utils/helpers';
 const { Text } = Typography;
 
@@ -13,8 +15,22 @@ export default function Movies() {
   const [size, setSize] = useState(10);
   const [year, setYear] = useState(null);
   const [winner, setWinner] = useState(null);
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+
   const queryParams = { page, size, year, winner };
   const queryString = constructQueryString(queryParams);
+
 
   const { data, isLoading, isError } = useQuery({
     queryFn: () => axios.get(`${BASE_API_URL}?${queryString}`).then(res => res.data),
@@ -46,13 +62,95 @@ export default function Movies() {
 
   const { totalElements } = data || {};
 
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1677ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
   const columns = [
     { title: 'Id', dataIndex: 'id', key: 'id' },
     {
       title: "Year",
       dataIndex: "year",
       key: "year",
-      filters: Array.from(new Set(data?.content?.map(movie => movie.year))).map(year => ({ text: year, value: year })),
+      ...getColumnSearchProps('year'),
     },
     { title: 'Title', dataIndex: "title", key: "title" },
     {
