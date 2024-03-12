c4eedfd047ab4341df465c685986fb4bbe7bea4b
Felipe Silva <felipesilva2004@hotmail.com>
2024-03-12 10:44:11 -0300

    refactor: move query server to _app.js file

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,31 +1,12 @@
 'use client';
 import { Flex } from 'antd'
 import Layout from '@/components/layout';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import YearsWithMultipleWinnersTable from '@/components/YearsWithMultipleWinnersTable';
 import TopThreeStudiosWithWinnersTable from '@/components/TopThreeStudiosWithWinnersTable';
 import ProducersWithLongestAndShortest from '@/components/ProducersWithLongestAndShortest';
 import ListMovieWinnersByYear from '@/components/ListMovieWinnersByYear';
 
-export default function App() {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        retry: 1,
-        keepPreviousData: true,
-      },
-    },
-  })
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Dashboard />
-    </QueryClientProvider>
-  );
-
-}
-
-function Dashboard() {
+export default function Dashboard() {
   return (
     <Layout>
       <Flex gap='large'>
