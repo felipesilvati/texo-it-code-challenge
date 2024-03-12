a98200bdf585b159fee4add30b0da1edd0c000af
Felipe Silva <felipesilva2004@hotmail.com>
2024-03-12 11:47:28 -0300

    feat: simple filter on year

--- frontend/src/pages/movies.js
+++ frontend/src/pages/movies.js
@@ -9,16 +9,24 @@ const { Text } = Typography;
 
 export default function Movies() {
   const [page, setPage] = useState(0);
+  const [year, setYear] = useState(null);
   const { data, isLoading, isError } = useQuery({
     queryFn: () => axios
-      .get(`${BASE_API_URL}?page=${page}&size=10`)
+      .get(`${BASE_API_URL}?page=${page}&size=10${year ? `&year=${year}` : ''}`)
       .then((res) => res.data),
-    queryKey: ['listMovies', page],
+    queryKey: ['listMovies', page, year],
     onError: (error) => console.error(error),
   });
 
   const handleTableChange = (pagination, filters, sorter) => {
+    console.log({ pagination, filters, sorter });
     setPage(pagination.current - 1);
+    if (filters?.year) {
+      setYear(filters.year[0])
+      setPage(0)
+    } else {
+      setYear(null)
+    }
   };
 
   if (isLoading) {
@@ -33,7 +41,12 @@ export default function Movies() {
 
   const columns = [
     { title: 'Id', dataIndex: 'id', key: 'id' },
-    { title: "Year", dataIndex: "year", key: "year" },
+    {
+      title: "Year",
+      dataIndex: "year",
+      key: "year",
+      filters: Array.from(new Set(data?.content?.map(movie => movie.year))).map(year => ({ text: year, value: year })),
+    },
     { title: 'Title', dataIndex: "title", key: "title" },
     { title: 'Winner?', dataIndex: "winner", key: "winner", render: (winner) => winner ? 'Yes' : 'No' },
   ];
