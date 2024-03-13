800d0334ce9c4b50a77b9febc7c6ce0009412f53
Felipe Silva <felipesilva2004@hotmail.com>
2024-03-12 22:26:00 -0300

    feat: show pagination even on single page to see total search results

--- frontend/src/pages/movies.js
+++ frontend/src/pages/movies.js
@@ -117,7 +117,7 @@ export default function Movies() {
         <Table
           dataSource={data?.content}
           columns={columns}
-          pagination={{ hideOnSinglePage: true, total: totalElements, showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} movies` }}
+          pagination={{ total: totalElements, showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} movies` }}
           onChange={handleTableChange}
         />
       </Card >
